ad22b3ee8ade73e752ffc0185f1df2b5da050530
18810777688 <kevin_loo@live.cn>
2022-02-25 02:26:22 +0800

    add file log

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@ const { app, Tray, Menu, nativeImage } = require('electron')
 const path = require('path')
 const connect = require('connect')
 const serveStatic = require('serve-static')
+const fs = require('fs')
+
+function log(tag, ...messages) {
+    const time = new Date()
+    const content = `${time.toISOString()}: ${tag} ${messages.join(' ')}\n`
+    fs.appendFileSync(path.resolve(path.dirname(process.execPath), '..', 'static-viewer.log'), content)
+}
+
+for (let tag of ['debug', 'error', 'info']) {
+    log[tag] = (...messages) => log(tag, ...messages)
+}
 
 function parseArgv(argv) {
     const _ = argv.shift()
@@ -14,7 +25,7 @@ function parseArgv(argv) {
         else dir = arg
     }
     if (unknown.length > 0) {
-        console.error(`has unknown argv: ${unknown.join(' ')}`)
+        log.error(`has unknown argv: ${unknown.join(' ')}`)
     }
     return [cmd, dir]
 }
@@ -35,11 +46,11 @@ function handleHook(os, cmd) {
     const target = path.basename(process.execPath)
     if (cmd === '--squirrel-install' || cmd === '--squirrel-updated') {
         reg(['ADD', `"HKLM\\SOFTWARE\\Classes\\Directory\\shell\\StaticViewer"`, '/ve', '/t', 'REG_SZ', '/d', `"Serve with StaticViewer"`, '/f'], err => {
-            if (err) console.error(err)
+            if (err) log.error(err)
             reg(['ADD', `"HKLM\\SOFTWARE\\Classes\\Directory\\shell\\StaticViewer"`, '/v', 'Icon', '/t', 'REG_SZ', '/d', `"${process.execPath}"`, '/f'], err => {
-                if (err) console.error(err)
+                if (err) log.error(err)
                 reg(['ADD', `"HKLM\\SOFTWARE\\Classes\\Directory\\shell\\StaticViewer\\command"`, '/ve', '/t', 'REG_SZ', '/d', `"\\\"${process.execPath}\\\" \\\"%1\\\""`, '/f'], err => {
-                    if (err) console.error(err)
+                    if (err) log.error(err)
                     update(['--createShortcut=' + target + ''], app.quit)
                 })
             })
@@ -48,7 +59,7 @@ function handleHook(os, cmd) {
     }
     if (cmd === '--squirrel-uninstall') {
         reg(['DELETE', `"HKLM\\SOFTWARE\\Classes\\Directory\\shell\\StaticViewer"`, '/f'], err => {
-            if (err) console.error(err)
+            if (err) log.error(err)
             update(['--removeShortcut=' + target + ''], app.quit)
         })
         return true
@@ -61,6 +72,8 @@ function handleHook(os, cmd) {
 }
 
 app.whenReady().then(() => {
+    log.info(`App launch with argv ${process.argv.join(' ')}`)
+
     const [cmd, dir] = parseArgv(process.argv)
     const os = process.platform
     if (handleHook(os, cmd)) return
@@ -69,10 +82,10 @@ app.whenReady().then(() => {
     const port = 50000 + Math.floor(Math.random() * 10000)
     const server = connect().use(serveStatic(folder)).listen(port, err => {
         if (err) {
-            console.error(err)
+            log.error(err)
             app.quit()
         }
-        console.log(`folder ${folder} started on ${port}`)
+        log.info(`folder ${folder} started on ${port}`)
         
         const addr = server.address()
         const icon = nativeImage.createFromPath(path.resolve(__dirname, 'icon.png'))
@@ -86,7 +99,7 @@ app.whenReady().then(() => {
                 enabled: true,
                 click: () => {
                     server.close(() => {
-                        console.log(`folder ${folder} stopped`)
+                        log.info(`folder ${folder} stopped`)
                         tray.destroy()
                         app.quit()
                     })
